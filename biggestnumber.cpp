f47e4ce400a1cf781307a5f3beef626443668dea
Rey Almaraz <ChrisRey@wlan-t-195-63-dhcp.CSUChico.EDU>
2018-02-06 11:08:36 -0800

    Declared two variables

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -7,10 +7,17 @@ int main()
 {
   
   cout<<"Please enter a whole number:\n";
-  
+
+  int WholeNumber1;
+   cin>> WholeNumber1;
+
   cout<<"Please enter another whole number:\n";
 
+  int WholeNumber2;
+  cin>> WholeNumber2;	
+
   cout<<"Of those two numbers, the biggest is: ";
+  
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
